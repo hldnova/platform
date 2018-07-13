f0bf657e25f66699681aafa661570f3244013e29
adamperlin <adamp@nanosoft.com>
2018-07-13 11:08:07 -0700

    Add additional flux regex test with regex as user function arg

--- query/functions/query_test.go
+++ query/functions/query_test.go
@@ -24,6 +24,7 @@ var skipTests = map[string]string{
 	"string_max":                "error: invalid use of function: *functions.MaxSelector has no implementation for type string (https://github.com/influxdata/platform/issues/224)",
 	"null_as_value":             "null not supported as value in influxql (https://github.com/influxdata/platform/issues/353)",
 	"difference_panic":          "difference() panics when no table is supplied",
+	"filter_by_regex_function":  "Regex as function argument not working in flux (https://github.com/influxdata/platform/issues/408)",
 }
 
 func Test_QueryEndToEnd(t *testing.T) {
