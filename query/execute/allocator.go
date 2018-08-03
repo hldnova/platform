d810ae4efc49b7f135ea3e302b1e1502bd48bc7e
Adam <adam@influxdata.com>
2018-08-03 17:01:45 -0400

    Added GrowXX functions to allocator.go

--- query/execute/allocator.go
+++ query/execute/allocator.go
@@ -71,6 +71,20 @@ func (a *Allocator) AppendBools(slice []bool, vs ...bool) []bool {
 	return s
 }
 
+func (a *Allocator) GrowBools(slice []bool, n int) []bool {
+	newCap := len(slice) + n
+	if newCap < cap(slice) {
+		return slice[:newCap]
+	}
+	// grow capacity same way as built-in append
+	newCap = newCap*3/2 + 1
+	s := make([]bool, newCap)
+	copy(s, slice)
+	diff := cap(s) - cap(slice)
+	a.account(diff, boolSize)
+	return s
+}
+
 // Ints makes a slice of int64 values.
 func (a *Allocator) Ints(l, c int) []int64 {
 	a.account(c, int64Size)
@@ -83,6 +97,20 @@ func (a *Allocator) AppendInts(slice []int64, vs ...int64) []int64 {
 		return append(slice, vs...)
 	}
 	s := append(slice, vs...)
+	diff := cap(s) - cap(slice)
+	a.account(diff, int64Size)
+	return s
+}
+
+func (a *Allocator) GrowInts(slice []int64, n int) []int64 {
+	newCap := len(slice) + n
+	if newCap < cap(slice) {
+		return slice[:newCap]
+	}
+	// grow capacity same way as built-in append
+	newCap = newCap*3/2 + 1
+	s := make([]int64, newCap)
+	copy(s, slice)
 	diff := cap(s) - cap(slice)
 	a.account(diff, int64Size)
 	return s
@@ -105,6 +133,20 @@ func (a *Allocator) AppendUInts(slice []uint64, vs ...uint64) []uint64 {
 	return s
 }
 
+func (a *Allocator) GrowUints(slice []uint64, n int) []uint64 {
+	newCap := len(slice) + n
+	if newCap < cap(slice) {
+		return slice[:newCap]
+	}
+	// grow capacity same way as built-in append
+	newCap = newCap*3/2 + 1
+	s := make([]uint64, newCap)
+	copy(s, slice)
+	diff := cap(s) - cap(slice)
+	a.account(diff, uint64Size)
+	return s
+}
+
 // Floats makes a slice of float64 values.
 func (a *Allocator) Floats(l, c int) []float64 {
 	a.account(c, float64Size)
@@ -117,6 +159,20 @@ func (a *Allocator) AppendFloats(slice []float64, vs ...float64) []float64 {
 		return append(slice, vs...)
 	}
 	s := append(slice, vs...)
+	diff := cap(s) - cap(slice)
+	a.account(diff, float64Size)
+	return s
+}
+
+func (a *Allocator) GrowFloats(slice []float64, n int) []float64 {
+	newCap := len(slice) + n
+	if newCap < cap(slice) {
+		return slice[:newCap]
+	}
+	// grow capacity same way as built-in append
+	newCap = newCap*3/2 + 1
+	s := make([]float64, newCap)
+	copy(s, slice)
 	diff := cap(s) - cap(slice)
 	a.account(diff, float64Size)
 	return s
@@ -137,6 +193,20 @@ func (a *Allocator) AppendStrings(slice []string, vs ...string) []string {
 		return append(slice, vs...)
 	}
 	s := append(slice, vs...)
+	diff := cap(s) - cap(slice)
+	a.account(diff, stringSize)
+	return s
+}
+
+func (a *Allocator) GrowStrings(slice []string, n int) []string {
+	newCap := len(slice) + n
+	if newCap < cap(slice) {
+		return slice[:newCap]
+	}
+	// grow capacity same way as built-in append
+	newCap = newCap*3/2 + 1
+	s := make([]string, newCap)
+	copy(s, slice)
 	diff := cap(s) - cap(slice)
 	a.account(diff, stringSize)
 	return s
